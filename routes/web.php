0f7401ec83c6cb8670d5de34c34e5adda39addf6
sa0aaa <safananda.33423123@mhs.polines.ac.id>
2025-05-18 22:32:11 +0700

    frontend only

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,24 @@ use App\Enums\UserRole;
 use App\Http\Controllers\VacanciesController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', [VacanciesController::class, 'index'])->name('home');
 
 // Redirect based on role
 Route::middleware(['auth', 'verified'])->get('/redirect', function () {
     return Auth::user()->role === UserRole::HR
-    ? redirect()->route('admin.dashboard')
-    : redirect()->route('user.info');
+        ? redirect()->route('admin.dashboard')
+        : redirect()->route('user.info');
 })->name('dashboard');
+
+Route::get('/candidate-status', function () {
+    return Inertia::render('candidate/profile/user/candidate-status');
+});
+
+Route::get('/nav-user', function () {
+    return Inertia::render('components/ui/nav-user');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
